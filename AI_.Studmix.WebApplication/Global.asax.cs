2850bee3fc49457e999d367402f67c79ec8ed7c4
AI_ <AI_@78274f13-e6b5-43e6-ba8c-8abe4aae97d1>
2011-09-18 18:18:12 +0000

    Внедрение зависимотей.

--- AI_.Studmix.WebApplication/Global.asax.cs
+++ AI_.Studmix.WebApplication/Global.asax.cs
@@ -1,15 +1,18 @@
 ﻿using System.Data.Entity;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using AI_.Studmix.WebApplication.DAL;
 using AI_.Studmix.WebApplication.Filters;
+using AI_.Studmix.WebApplication.dependencies;
+using Microsoft.Practices.Unity;
 
 namespace AI_.Studmix.WebApplication
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
@@ -21,11 +24,12 @@ namespace AI_.Studmix.WebApplication
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-
+                "Default",
+                // Route name
+                "{controller}/{action}/{id}",
+                // URL with parameters
+                new {controller = "Home", action = "Index", id = UrlParameter.Optional} // Parameter defaults
+                );
         }
 
         private static void InitializeDatabase()
@@ -43,8 +47,22 @@ namespace AI_.Studmix.WebApplication
 
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
-
+            RegisterDependencyResolver();
             InitializeDatabase();
+        }
+
+        private void RegisterDependencyResolver()
+        {
+            var container = new UnityContainer();
+            ConfigureContainer(container);
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+
+        private void ConfigureContainer(IUnityContainer container)
+        {
+            container.RegisterType<IControllerFactory, DefaultControllerFactory>();
+            container.RegisterType<IControllerActivator, ControllerActivator>();
+            container.RegisterType<IViewPageActivator, ViewPageActivator>();
         }
     }
 }
---
+++ AI_.Studmix.WebApplication/dependencies/UnityDependencyResolver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Microsoft.Practices.Unity;
+
+namespace AI_.Studmix.WebApplication.dependencies
+{
+    public class UnityDependencyResolver : IDependencyResolver
+    {
+        private IUnityContainer _container;
+        public UnityDependencyResolver(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _container.Resolve(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _container.ResolveAll(serviceType);
+        }
+    }
+}
