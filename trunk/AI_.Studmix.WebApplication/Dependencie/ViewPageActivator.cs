b0118374bfb1a02f3b0dfeef3f7996d09a70f74c
AI_ <AI_@78274f13-e6b5-43e6-ba8c-8abe4aae97d1>
2011-09-29 14:03:26 +0000

    Оргиназиация солюшена.

---
+++ trunk/AI_.Studmix.WebApplication/Dependencie/ViewPageActivator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace AI_.Studmix.WebApplication.dependencies
+{
+    public class ViewPageActivator : IViewPageActivator
+    {
+        public object Create(ControllerContext controllerContext, Type type)
+        {
+            return DependencyResolver.Current.GetService(type);
+        }
+    }
+}
