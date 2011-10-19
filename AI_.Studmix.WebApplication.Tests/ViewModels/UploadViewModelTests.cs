1dcfd982d7f7d9b18d2a863e91252b1911b8dc0f
AI_ <AI_@78274f13-e6b5-43e6-ba8c-8abe4aae97d1>
2011-10-19 16:40:55 +0000

    Управление контентом. Валидация.

---
+++ AI_.Studmix.WebApplication.Tests/ViewModels/UploadViewModelTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AI_.Studmix.WebApplication.ViewModels.Content;
+using Xunit;
+using FluentAssertions;
+
+namespace AI_.Studmix.WebApplication.Tests.ViewModels
+{
+    public class UploadViewModelTests
+    {
+        [Fact]
+        public void Validate_NoFilesPosted_ModelStateErrorRegistered()
+        {
+            // Arrange
+            var viewModel = new UploadViewModel();
+            viewModel.ContentFiles = new List<HttpPostedFileBase> { null };
+
+            // Act
+            var results = viewModel.Validate(null);
+
+            // Assert
+            results.Should().Contain(x => x.MemberNames.Contains("ContentFiles"));
+        }
+    }
+}
